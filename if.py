f687718e1101d5878a8fa397a4ac92f7c0fe6cad
pavithrapavithra13461-cmyk <pavithrapavithra13461@gmail.com>
2025-11-07 14:57:59 +0530

    Add files via upload

---
+++ if.py
@@ -0,0 +1,19 @@
+if 5<4:
+    print("N")
+if 4<5:
+    print("Y")
+if"name"=="NAME":
+    print("O")
+if 10%2==0:
+    print("E")
+if 10%3==0:
+    print("T")
+if(4*6)==24:
+    print("S")
+food =input("what's the best food?:")
+if food=="rice":
+    print("Yes,rice is the best.")
+elif food=="apple":
+    print("Apples aren't my cup of rice.")
+else:
+    print("Never heard of it!")
